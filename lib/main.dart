4442fad41141be43dd92591c3fc9dc42ed22b1d6
yury morozov <morozovypa1@gmail.com>
2022-04-08 11:26:09 +0800

    deleted cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_firebase_app/logic/bloc/auth_bloc.dart';
-import 'package:flutter_firebase_app/logic/cubit/auth_cubit.dart';
+import 'package:flutter_firebase_app/logic/bloc/auth_bloc.dart'; 
 import 'package:flutter_firebase_app/router/router.dart';
 import 'package:flutter_firebase_app/service/auth_service.dart';
 
